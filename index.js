ab2267e34ad95ee8f3a08b99cd9e766cdc063b9f
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-08-03 15:08:32 -0300

    Create Google OAuth Strategy

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const passport = require('passport');
 
 const app = express();
 
-require('./routes/auth')(app);
+require('./services/passport'); // Run passport strategies
+require('./routes/auth')(app); // Run auth routes handles
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,18 @@
+const passport = require('passport');
+
 module.exports = app => {
 	app.get('/', (req, res) => {
 		res.send({ hello: 'World!' });
 	});
 
-	app.get('/auth/google', (req, res) => {});
+	// Route - Enter Google OAuth flow
+	app.get(
+		'/auth/google',
+		passport.authenticate('google', {
+			scope: ['profile', 'email']
+		})
+	);
+
+	// Route - Callback returning from Google OAuth flow
+	app.get('/auth/google/callback', passport.authenticate('google'));
 };
